f5408796963be158ed36445985c214350f61ceb5
GNtousakis <greco1996@gmail.com>
2020-05-20 13:45:27 +0300

    Add script to install zsh

---
+++ scripts/installZsh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Zsh and make it default
+sudo apt-get install zsh -y
+chsh -s $(which zsh)
+
+# Install OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install zsh-autosuggestions, zsh-syntax-highlighting 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# TODO: Install fonts before installing theme
+
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
