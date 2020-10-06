eb397a700c6b1775021366df6583ec6670b11e0d
GNtousakis <greco1996@gmail.com>
2020-10-06 18:58:10 +0300

    Add multiple screens

---
+++ scripts/multipleScreens.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ./mus.sh <session-name> <user>
+
+# https://unix.stackexchange.com/a/163878
+if [ "$#" -eq 2 ]; then
+  # if /var/run/screen not 755
+  # sudo chmod 755 /var/run/screen
+  # sudo rm -fr /var/run/screen/*
+
+  screen -d -m -S $1
+  screen -S $1 -X multiuser on
+  screen -S $1 -X acladd $2
+  echo "screen -x $(whoami)/$1"
+else
+  echo "./$0 <session-name> <user>"
+fi
